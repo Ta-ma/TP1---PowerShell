d962c60551945041b4ee6fb2f204503c1304e5e8
Ta-ma <santiago.tamashiro@gmail.com>
2018-04-25 00:08:04 -0300

    agregado ejercicios 3 y 5

---
+++ Ejercicio 3/Ejercicio3.ps1
@@ -0,0 +1,75 @@
+﻿<#
+.SYNOPSIS
+     El Script creara un Job que al cerrarse el proceso indicado corrige el archivo indicado. 
+     Ejercicio [-DireccionArchivo] <String> [-Log] <String> [-Proceso] <String>
+.DESCRIPTION
+    El Script creara un Job que quedara pendiente de creacion del proceso indicado [-Proceso] y aguardara su detencion para asi leer y corregir el archivo indicado [-DireccionArchivo], dicha correcion se escribira en el arhivo de nombre similar pero cuya terminacion sera "_modif". 
+    Luego guardada un archivo de Log que contendra la fecha y hora en que se corrigio el archivo, el nombre del archivo y la cantidad de caracteres modificados
+
+.EXAMPLE
+    Ejercicio3 -DireccionArchivo C:\Archivo.txt -Log C:\Log.txt -Proceso "Notepad.exe"
+.EXAMPLE
+    Ejercicio3 -DireccionArchivo C:\Archivo.txt -Log .\Log.txt -Proceso "Notepad"
+.EXAMPLE
+    Ejercicio3 -DireccionArchivo .\Archivo.txt -Log C:\Log.txt -Proceso "Notepad"
+.EXAMPLE
+    Ejercicio3 -DireccionArchivo .\Archivo.txt -Log .\Log.txt -Proceso "Notepad.exe"
+
+#>
+Param([Parameter(Mandatory=$true)][string]$DireccionArchivo
+     ,[Parameter(Mandatory=$true)][string]$Log
+     ,[Parameter(Mandatory=$true)][string]$proceso)
+
+if(([System.IO.Path]::IsPathRooted($DireccionArchivo)) -eq $false){$DireccionArchivo = (Get-ChildItem).DirectoryName+'\'+$DireccionArchivo.Substring(1)}
+if(([System.IO.Path]::IsPathRooted($Log)) -eq $false){$Log = (Get-ChildItem).DirectoryName+'\'+$Log.Substring(0)}
+ 
+
+$comando = { 
+Param([string]$DireccionArchivo
+     ,[string]$Log
+     ,[string]$proceso)
+    if($proceso.Contains(".")){$process = $proceso.Remove($proceso.LastIndexOf("."))} else {$process = $proceso}
+    
+    while($true)
+    {
+        if((Get-Process).Name.ToUpper().Contains($process.ToUpper()))
+        {
+            Wait-Process -Name $process
+            $cant=0
+            $line=""
+    
+            (Get-Content $DireccionArchivo).Split("`n") | ForEach{
+                if($line.Length -ne 0){$line += "`r`n"}
+                $_.Split('.') | ForEach{
+                    [string]$aux=$_
+                    while($aux[0] -eq ' ')
+                    {
+                        $line += ' '
+                        $aux=$aux.Substring(1)
+                    }
+                    if($aux[0] -ge 97 -and $aux[0] -le 126)
+                    { 
+                        $cant++
+                        $line += ([string]$aux[0]).ToUpper()
+                        if($aux.Length -ge 2)
+                        {
+                            $line+=$aux.Substring(1)
+                        }
+                    }
+                    else
+                    {
+                        $line += $aux
+                    }
+                    if(-not $line.EndsWith('.'))
+                    {
+                        $line+='.'
+                    }
+                } 
+            }
+            $line | Out-File $DireccionArchivo.replace('.','_modif.') -NoNewline
+            (Get-Date -Format "yyyy/mm/dd - hh:mm:ss  " )+ $DireccionArchivo.subString($DireccionArchivo.lastindexof('\')+1) + "  $cant"  >> $Log  
+        }
+    }   
+} 
+Start-Job -ScriptBlock $comando -ArgumentList $DireccionArchivo, $Log, $proceso -Name "Espero a $Proceso"
+
---
+++ Ejercicio 5/Ejercicio5.ps1
@@ -0,0 +1,64 @@
+﻿<#
+.SYNOPSIS
+     El Script mostrara por pantalla los procesos enviados a travez del parametro -Path (posicion 1)
+     Ejercicio5 [-Path] <script> [[-K [-F]][-U][-C]]
+.DESCRIPTION
+    El Script creara una tabla con los datos solicitados por el usuario a travez de -K/-U/-C (exclusivamente una) 
+
+.EXAMPLE
+    Ejercicio5 -path C:\Archivo.txt -K [-F]
+    esta opcion detendra la ejecucion de los procesos que esten dentro del archivo enviado atravez de -Path. El modificador -F fuerza la detencion
+.EXAMPLE
+    Ejercicio5 -path Archivo.txt -U
+    El script mostrara el PID, el nombre, el porcentaje de uso de CPU, el consumo de memoria y el consumo maximo de memoria.
+.EXAMPLE
+    Ejercicio5 -.\Archivo.txt  -C
+    El script mostrara el PID, el nombre, el porcentaje de uso de CPU.
+#>
+Param([Parameter(Mandatory=$true, position=1)][ValidateScript({Test-Path $_ })][string]$path,
+      [Parameter()]#ParameterSetName="Set1")]
+      [switch]$U,
+      [Parameter()]#ParameterSetName="Set2")]
+      [switch]$C,
+      [Parameter()]#ParameterSetName="Set3")]
+      [switch]$K,
+      [Parameter()]
+      [switch]$F
+      )
+if(-not($C -xor ($U -xor $K))){ (Get-Date -Format "yyyy/mm/dd - HH:mm:ss") + " | No se enviaron correctamente los parametros."  >> "Log.txt"
+                                throw [System.ArgumentException]::New('No se enviaron correctamente los parametros. Puede visualizar la ayuda y ejemplos del Script utilizando get-help')}
+if($F -and -not $K){
+throw [System.ArgumentException]::New('El paramero -F solo puede ser utilizado para forzar la detencion de procesos... por lo tanto solo es posible la combinacion -K -F')}
+#inicio las variables para tenerlas globalmente
+$mipid=$micpu=$miram=$miramM=$nombre=$mensaje=""
+$tabla=@()
+(Get-Content $path).Replace('.exe','') | foreach{try{
+    $nombre=$_
+    $hora=(Get-Date -Format "yyyy/mm/dd - HH:mm:ss")
+    if($K){    if($F) {Stop-Process -Name $_ -Force -ErrorAction Stop} 
+               else   {Stop-Process -Name $_ -ErrorAction Stop}} 
+    #se opto por usar el caracter comodin * para en caso de tener mas de un proceso con el mismo nombre tambien se incluya en la tabla
+    else      {$mipid = (Get-Counter "\Proceso($_*)\Id. de proceso" -ErrorAction Stop).CounterSamples.CookedValue
+               $micpu = (Get-Counter "\Proceso($_*)\% de tiempo de procesador" -ErrorAction Stop).CounterSamples.CookedValue
+        if($U){$miram = (Get-Counter "\Proceso($_*)\espacio de trabajo" -ErrorAction Stop).CounterSamples.CookedValue 
+               $miramM= (Get-Counter "\Proceso($_*)\Uso máximo del espacio de trabajo" -ErrorAction Stop).CounterSamples.CookedValue}}
+        $i=0   
+                            
+        if($C){ $mipid | foreach{ $tabla += New-Object PSObject -Property @{Nombre=(Get-Process -Id $_).Name.ToString() ;ID=$_ ;Uso_CPU=$micpu[$i++]}}}
+        if($U){ $mipid | foreach{ $tabla += New-Object PSObject -Property @{Nombre=(Get-Process -Id $_).Name.ToString() ;ID=$_ ;Uso_CPU=$micpu[$i] ;Uso_De_Memoria=$miram[$i]/1024000 ;Uso_Maximo_De_Memoria=$miramM[$i++]/1024000}}}
+    }catch{ $error=$_  #atrapo la exepcion para poder cargarlo al archivo log.txt que se encuentra en la ruta de ejecucion
+            $mipid|foreach{if($error.exception.message -eq "No se puede enlazar el argumento al parámetro 'Id' porque es nulo.")
+                                {$mensaje+="$hora | El proceso indicado ($nombre) no es valido o no se encuentra ejecutando`r`n"}
+                           else{                 
+                                if($_ -ne $null){$mensaje+="$hora | "+$error.exception.message+"`r`n"}
+                                else            {$mensaje+="$hora | "+$error.exception.message+"`r`n"}}}}
+}
+
+if($mensaje.length -ne 0){ 
+    $mensaje | Out-File "log.txt" -Append -NoNewline
+    $mensaje
+}
+
+#formateo la salida, este paso se puede omitir... es simplemente por estetica... una salida mas simple seria "return $tabla" o simplemente "$tabla" para unicamente mostrarla por pantalla
+if($C) {return $tabla | Format-Table -Property ID, Nombre, Uso_CPU}
+if($U) {return $tabla | Format-Table -Property ID, Nombre, Uso_CPU, Uso_De_Memoria, Uso_Maximo_De_Memoria}
